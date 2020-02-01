5ffae193a5a5c5d84fb1d10a4a80c533c390fd48
miguel <miguel.junior.mat@hotmail.com>
2020-02-01 11:04:19 -0300

    criando as linhas e colunas

--- jogo_da_velha/jogo.js
+++ jogo_da_velha/jogo.js
@@ -1,1 +1,37 @@
-var tic_tac_toe = {};
+const tic_tac_tow = {
+
+    board: ["","","","","","","","",""],
+    simbols: {
+        options: ["x","o"],
+        turn_index: 0,
+        change: function () {
+            this.turne_index = (this.turn_index === 0 ? 1 : 0);
+        },
+    },
+    container_element: null,
+    gameover: false,
+
+
+    init: function (container) {
+        this.container_element = container;
+
+    },
+
+    make_play: function (position) {
+        if (this.gameover) return false;
+        if (this.board[position] === "") {
+            this.board[position] = this.simbols
+        }
+    },
+
+    draw: function () {
+        let content = "";
+
+        for (i in this.board) {
+            content += '<div onclick = "tic_tac_tow.make_play('+i+')">'+ this.board[i] + '</div>';
+
+            this.container_element.innerHTML = content;
+        };
+
+    },
+};
